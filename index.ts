42bb3f8e93de4efc91d1365a0f77bde24fc2c50e
Jose Fabio <J053Fabi0@users.noreply.github.com>
2024-05-11 14:40:36 -0600

    Remove unused code

--- index.ts
+++ index.ts
@@ -1,13 +1,1 @@
-import login from "./login";
 import "./telegram/initBot";
-import getBrowser from "./utils/browser";
-import sleep from "./utils/sleep";
-
-// (async () => {
-//   const browser = await getBrowser();
-//   const page = await browser.newPage();
-//   const loggedIn = await login(page);
-//   console.log(loggedIn);
-//   await sleep(10000);
-//   await browser.close();
-// })();
