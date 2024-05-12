0350495874ab2b55f92cb14c1b593b9e2fb47daa
Jose Fabio <J053Fabi0@users.noreply.github.com>
2024-05-12 13:30:20 -0600

    Try to login at most 3 times

--- login.ts
+++ login.ts
@@ -5,29 +5,26 @@ import sleep from "./utils/sleep";
 import bot from "./telegram/initBot";
 import checkElement from "./utils/checkElement";
 
-function notLoggedIn() {
-  return bot.api.sendMessage(env.ADMIN_ID, "Login to Evaa Finance. /login");
-}
-
 export default async function login(page: Page) {
-  await page.goto("https://app.evaa.finance");
-  const session = await fs.promises.readFile("session.json", "utf8").catch(() => null);
+  for (let i = 0; i < 3; i++) {
+    await page.goto("https://app.evaa.finance");
+    const session = await fs.promises.readFile("session.json", "utf8").catch(() => null);
 
-  if (!session) {
-    await notLoggedIn();
-    return false;
+    if (!session) continue;
+
+    await page.session.restoreString(session);
+    await page.reload();
+
+    await sleep(1);
+
+    const isLoggedIn = !(await checkElement(page, "::-p-xpath(//button[contains(., 'Skip to the App')])", {
+      timeout: 1000,
+    }));
+
+    if (!isLoggedIn) continue;
+    return isLoggedIn;
   }
 
-  await page.session.restoreString(session);
-  await page.reload();
-
-  await sleep(2);
-
-  const isLoggedIn = !(await checkElement(page, "::-p-xpath(//button[contains(., 'Skip to the App')])", {
-    timeout: 1000,
-  }));
-
-  if (!isLoggedIn) await notLoggedIn();
-
-  return isLoggedIn;
+  await bot.api.sendMessage(env.ADMIN_ID, "Login to Evaa Finance. /login");
+  return false;
 }
